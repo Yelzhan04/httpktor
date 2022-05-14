9f8d6218fa8b62a36df0814241cc7f4fc5fe674e
Yelzhan04 <eljan00.kz@gmail.com>
2022-05-14 17:58:48 +0600

    first commit

---
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -0,0 +1,26 @@
+package com.example.plugins
+
+import com.example.models.customerStorage
+import com.example.routes.customerRouting
+import com.example.routes.getOrderRoute
+import com.example.routes.listOrdersRoute
+import com.example.routes.totalizeOrderRoute
+import io.ktor.server.routing.*
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.request.*
+
+fun Application.configureRouting() {
+
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+        customerRouting()
+        listOrdersRoute()
+        getOrderRoute()
+        totalizeOrderRoute()
+
+    }
+}
